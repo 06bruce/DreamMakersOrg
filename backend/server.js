026657475e30b1b7c70a720096bb63d58bef1a2e
06bruce <brucenshuti2@gmail.com>
2025-07-29 15:47:02 +0200

    UMS Full Dockerized

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/admin', adminRoutes);
 app.use ('/api/users', userRoutes);
 
 // Connect to MongoDB (update with your URI)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('📅 MongoDB connected'))
   .catch(err => console.error(err));
 
